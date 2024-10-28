149ae574425152b5f9bac9763318d591605e7f00
Samuel Teigland <samuelteigland@websiteartificers.com>
2024-10-27 21:10:12 -0500

    added a hamburger menu

--- src/components/global/navbar/Navbar.jsx
+++ src/components/global/navbar/Navbar.jsx
@@ -1,12 +1,48 @@
 import './navbar.css'
-import { MainLogo } from '../../.././assets/assets';
+import { MainLogo, Hamburger } from '../../.././assets/assets';
+import { useState } from 'react';
 
 export default function Navbar() {
+    const [ isVisible, setVisible ] = useState(false);
+
     return (
-        <div className='navbar__container'>
-            <a href='/' className='navbar__logo--container'>
-                <MainLogo />
-            </a>
+        <div className='navbar'>
+            <div className='navbar__container'>
+                <a href='/' className='navbar__logo--container'>
+                    <MainLogo />
+                </a>
+                <div className='navbar__link--container'>
+                    <a href='#'>
+                        Greenhill Academy
+                    </a>
+                    <a href='https://www.zeffy.com/donation-form/a166cdf1-90c8-4fa1-a29a-97cf0dc59ba1'>
+                        Donate
+                    </a>
+                    <a
+                    target="_blank" rel="noopener noreferrer" href='mailto:mstebbe@fishersumc.org'>
+                        Contact Us
+                    </a>
+                    <div className='hamburger__container' onClick={
+                        () => {
+                            setVisible(!isVisible);
+                        }
+                    }>
+                        <Hamburger />
+                    </div>
+                </div>
+            </div>
+            <div className={ isVisible === true ? 'hamburger__link--container shown__menu' : 'hamburger__link--container hidden__menu' }>
+                <a href='#'>
+                    Greenhill Academy
+                </a>
+                <a href='https://www.zeffy.com/donation-form/a166cdf1-90c8-4fa1-a29a-97cf0dc59ba1'>
+                    Donate
+                </a>
+                <a
+                target="_blank" rel="noopener noreferrer" href='mailto:mstebbe@fishersumc.org'>
+                    Contact Us
+                </a>
+            </div>
         </div>
     )
 }
